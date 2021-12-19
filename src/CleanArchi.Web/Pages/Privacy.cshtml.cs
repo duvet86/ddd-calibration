58fda8129e82194315aa8f11999f28e5ed057655
Luca <duvet86@gmail.com>
2021-12-19 11:53:10 +0800

    Fixed razor pages

--- src/CleanArchi.Web/Pages/Privacy.cshtml.cs
+++ src/CleanArchi.Web/Pages/Privacy.cshtml.cs
@@ -4,8 +4,14 @@ namespace CleanArchi.Web.Pages;
 
 public class PrivacyModel : PageModel
 {
+    private readonly ILogger<PrivacyModel> _logger;
+
+    public PrivacyModel(ILogger<PrivacyModel> logger)
+    {
+        _logger = logger;
+    }
+
     public void OnGet()
     {
     }
 }
-
